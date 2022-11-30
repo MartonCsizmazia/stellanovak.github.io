cf5076b22352e8a1703e21d8aca84694cf074988
Marton Csizmazia <martoncsizmazia@voormedia.com>
2022-11-30 12:15:24 +0100

    language is stored in localstorage upon choosing.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,8 +13,10 @@ const Modal = (props) => {
   return (
     <div className="modal">
       <p>Choose your language: </p>
-      <button className="btn btn--alt" onClick={chooseHun}>Magyar</button>
-      <button className="btn" onClick={chooseEng}>English</button>
+      <div className="flags">
+        <div id="flag" onClick={chooseHun}><img id="flag-picture" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg" alt="" ></img></div>
+        <div id="flag" onClick={chooseEng}><img id="flag-picture" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Flag_of_the_United_Kingdom.png" alt=""></img></div>
+      </div>
     </div>
   )
 }
